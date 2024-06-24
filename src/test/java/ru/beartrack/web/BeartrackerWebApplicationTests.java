ce15242b9979b51d36fab99451d6de4dde95b6cc
IvanMTD <morgantdarck@gmail.com>
2024-06-24 22:10:14 +0300

    init commit

---
+++ src/test/java/ru/beartrack/web/BeartrackerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.beartrack.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeartrackerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
