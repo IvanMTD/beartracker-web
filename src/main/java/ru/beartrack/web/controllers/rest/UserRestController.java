330fa8805d7aa23f59ea6f0b2133179d26449648
Ivan Morgan <morgantdarck@gmail.com>
2024-06-28 16:20:32 +0300

    update 28.06.2024 * начата разработка валидации

--- src/main/java/ru/beartrack/web/controllers/rest/UserRestController.java
+++ src/main/java/ru/beartrack/web/controllers/rest/UserRestController.java
@@ -1,4 +1,20 @@
 package ru.beartrack.web.controllers.rest;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import ru.beartrack.web.services.ApplicationUserService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserRestController {
+    private final ApplicationUserService userService;
+    @GetMapping("/search/username")
+    public Mono<Boolean> isUsernameExist(@RequestParam(name = "request") String username){
+        return userService.findByUsername(username).flatMap(userDetails -> Mono.just(true)).switchIfEmpty(Mono.just(false));
+    }
 }
